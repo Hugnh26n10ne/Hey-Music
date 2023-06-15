ba2da2de97c98b5ca636a636af6fe8b7fb0d7e33
Đàm Đình hưng <117186163+Hugnh26n10ne@users.noreply.github.com>
2023-06-15 17:18:25 +0700

    sửa tên LayoutDefault sang DefaultLayout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
-import { LayoutDefault } from '~/components/Layout';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
     return (
@@ -10,7 +10,7 @@ function App() {
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
-                        let Layout = LayoutDefault;
+                        let Layout = DefaultLayout;
 
                         if (route.layout) {
                             Layout = route.layout;
