4ab4c8e1490caa2b53eaddc0becdef963579458f
Đàm Đình hưng <117186163+Hugnh26n10ne@users.noreply.github.com>
2023-06-15 23:38:09 +0700

    Xây dựng header #1

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+    logo1: require('~/assets/images/logo/logo-1.png'),
+    logo2: require('~/assets/images/logo/logo-2.png'),
+    notification: require('~/assets/images/notification/notification.svg').default,
+};
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,32 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('header-left')}>
+                <logo className={cx('logo')}>
+                    <img src={images.logo1} alt="logo" />
+                    Hey Music
+                </logo>
+            </div>
+            <div className={cx('header-center')}>
+                <search className={cx('search')}>
+                    <ion-icon name="search-outline"></ion-icon>
+                    <span>Tìm kiếm bài hát theo ý thích ...</span>
+                </search>
+            </div>
+            <div className={cx('header-right')}>
+                <notification className={cx('notification')}>
+                    <img data-index="0" src={images.notification} alt="notification" />
+                </notification>
+                <login className={cx('btn-login')}>
+                    <button>Đăng nhập</button>
+                </login>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
