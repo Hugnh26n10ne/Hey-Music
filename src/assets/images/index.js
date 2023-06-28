2f1eea4baa74626e24acf232c31b402542c993aa
Đàm Đình hưng <117186163+Hugnh26n10ne@users.noreply.github.com>
2023-06-28 22:36:06 +0700

    Xây dựng header #2

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,5 +2,6 @@ const images = {
     logo1: require('~/assets/images/logo/logo-1.png'),
     logo2: require('~/assets/images/logo/logo-2.png'),
     notification: require('~/assets/images/notification/notification.svg').default,
+    magnifier: require('~/assets/images/magnifier/magnifier.svg').default,
 };
 export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -6,24 +6,78 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('header-left')}>
-                <logo className={cx('logo')}>
-                    <img src={images.logo1} alt="logo" />
-                    Hey Music
-                </logo>
+                <img src={images.logo1} alt="logo" />
+                <div className={cx('logo-text')}>Hey Music</div>
             </div>
             <div className={cx('header-center')}>
-                <search className={cx('search')}>
+                <div className={cx('search', '')}>
                     <ion-icon name="search-outline"></ion-icon>
-                    <span>Tìm kiếm bài hát theo ý thích ...</span>
-                </search>
+                    <input className={cx('')} placeholder="Tìm kiếm bài hát theo ý thích ..."></input>
+                    <span className={cx('btn-close')}>&times;</span>
+                </div>
+                <ul className={cx('dropdown')}>
+                    <li className={cx('dropdown-item')}>
+                        <h4 className={cx('dropdown_1-title')}>Trending</h4>
+                        <ul className={cx('dropdown_1')}>
+                            <li className={cx('dropdown_1-item')}>
+                                <div className={cx('item-name')}>
+                                    <ion-icon name="chevron-forward-outline" className={cx('item-icon')}></ion-icon>
+                                    <span className={cx('item-title')}>Making My Way</span>
+                                </div>
+                                <span className={cx('item-author')}>Sơn Tùng MTP</span>
+                            </li>
+                            <li className={cx('dropdown_1-item')}>
+                                <div className={cx('item-name')}>
+                                    <ion-icon name="chevron-forward-outline" className={cx('item-icon')}></ion-icon>
+                                    <span className={cx('item-title')}>Making My Way</span>
+                                </div>
+                                <span className={cx('item-author')}>Sơn Tùng MTP</span>
+                            </li>
+                            <li className={cx('dropdown_1-item')}>
+                                <div className={cx('item-name')}>
+                                    <ion-icon name="chevron-forward-outline" className={cx('item-icon')}></ion-icon>
+                                    <span className={cx('item-title')}>Making My Way</span>
+                                </div>
+                                <span className={cx('item-author')}>Sơn Tùng MTP</span>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className={cx('dropdown-item')}>
+                        <h4 className={cx('dropdown_1-title')}>Trending</h4>
+                        <ul className={cx('dropdown_1')}>
+                            <li className={cx('dropdown_1-item')}>
+                                <div className={cx('item-name')}>
+                                    <ion-icon name="chevron-forward-outline" className={cx('item-icon')}></ion-icon>
+                                    <span className={cx('item-title')}>Making My Way</span>
+                                </div>
+                                <span className={cx('item-author')}>Sơn Tùng MTP</span>
+                            </li>
+                            <li className={cx('dropdown_1-item')}>
+                                <div className={cx('item-name')}>
+                                    <ion-icon name="chevron-forward-outline" className={cx('item-icon')}></ion-icon>
+                                    <span className={cx('item-title')}>Making My Way</span>
+                                </div>
+                                <span className={cx('item-author')}>Sơn Tùng MTP</span>
+                            </li>
+                            <li className={cx('dropdown_1-item')}>
+                                <div className={cx('item-name')}>
+                                    <ion-icon name="chevron-forward-outline" className={cx('item-icon')}></ion-icon>
+                                    <span className={cx('item-title')}>Making My Way</span>
+                                </div>
+                                <span className={cx('item-author')}>Sơn Tùng MTP</span>
+                            </li>
+                        </ul>
+                    </li>
+                    <div className={cx('more')}>
+                        <a href="xemthem">Xem thêm</a>
+                    </div>
+                </ul>
             </div>
             <div className={cx('header-right')}>
-                <notification className={cx('notification')}>
-                    <img data-index="0" src={images.notification} alt="notification" />
-                </notification>
-                <login className={cx('btn-login')}>
+                <i data-count="0" className={cx('notification', 'notify', 'fa fa-bell')} alt="notification"></i>
+                <div className={cx('btn-login')}>
                     <button>Đăng nhập</button>
-                </login>
+                </div>
             </div>
         </header>
     );
